00b1191d71b2799615143341f10d3134675ffcbd
shitakemura <shitakemura@gmail.com>
2023-05-18 16:47:20 +0900

    Add layouts to wrap a page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
-import { AppProvider } from '@/providers/app'
+import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import { ReactElement, ReactNode } from 'react'
+import { AppProvider } from '@/providers/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <AppProvider>
-      <Component {...pageProps} />
-    </AppProvider>
-  )
+type NextPageWithLayout = AppProps & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page)
+  const pageContent = getLayout(<Component {...pageProps} />)
+
+  return <AppProvider>{pageContent}</AppProvider>
 }
