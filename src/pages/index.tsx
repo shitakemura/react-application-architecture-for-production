2d263ee0b626a0c9f96e0e80a04eb633adbb53fb
shitakemura <shitakemura@gmail.com>
2023-05-19 14:59:44 +0900

    Update layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
-import { Center } from '@chakra-ui/react'
+import { Center, Heading, Text, VStack } from '@chakra-ui/react'
 
+import { Link } from '@/components/link'
 import { Seo } from '@/components/seo'
 
 export default function LandingPage() {
   return (
     <>
       <Seo title="Jobs App" />
-      <Center>{/* rest of the component */}</Center>
+      <Center flexDirection="column" h="full">
+        <VStack maxW="3xl" spacing="8">
+          <Heading size="3xl">Jobs App</Heading>
+          <Text fontSize={{ base: 'lg', md: 'xl' }} maxW="2xl" color="muted">
+            Manage your careers pages
+          </Text>
+          <Link href={'/dashboard/jobs'} variant="solid">
+            Get Started
+          </Link>
+        </VStack>
+      </Center>
     </>
   )
 }
