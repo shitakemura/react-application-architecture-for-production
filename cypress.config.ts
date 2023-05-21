4e9eaca20510d5815b5578a6b339c475edf33ad0
shitakemura <shitakemura@gmail.com>
2023-05-21 21:47:54 +0900

    Add e2e test for dashboard flow

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',
+    testIsolation: false, // MEMO: https://docs.cypress.io/guides/core-concepts/test-isolation
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
